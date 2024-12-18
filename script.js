777cf5b053719bf0172a87f060d0826a14393073
faraja maha <mahafaraja@gmail.com>
2024-12-18 04:39:56 -0800

    working on the bag that still couse the hamburger not to work

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const menu = document.querySelector(".menu-links");
+const icon = document.querySelector(".menu-links");
+    
     function toggleMenu()
     {
-        const menu = document.querySelector(".menu-links");
-        const icon = document.querySelector(".menu-links");
+        
         
         menu.classList.toggle("open");
         icon.classList.toggle("open");
