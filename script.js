d547538f27b667de2a9dce571c738475592fc3b0
faraja maha <mahafaraja@gmail.com>
2025-01-21 14:35:44 +0300

    finieshed all the section in .html file

--- script.js
+++ script.js
@@ -10,21 +10,3 @@
         menu.classList.toggle("open");
         icon.classList.toggle("open");
     }
-                // STYLE THE DOWNLOAD & CV BUTTONS
- function buttonHover(){
-    const buttons = document.querySelectorAll('.btn-color1','.btn-color2');
-
-    buttons.forEach( button => {
-        button.addEventListener( 'mouseenter' , () => {
-            buttons.forEach(otherButton => {
-                if (otherButton !== button){
-                    otherButton.style.backgroundColor = 'white';
-                    otherButton.style.color = 'black';
-                }
-            });
-            button.style.backgroundColor = 'black';
-            button.style.color = 'white';
-        });
-    });
-
- }
